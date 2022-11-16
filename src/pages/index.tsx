94844684f52b8e75beacf0cc22a0069c6ca0ca36
Murilo Pinheiro <pinheiromurilo13@gmail.com>
2022-11-15 21:33:32 -0300

    initial config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,16 @@ import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
 export default function Home(): JSX.Element {
+
+  const fetchImages = async ({pageParam = null}) => {
+    const res = await api.get('/api/images', { params: { after: pageParam } });    
+    return res.data;
+  }
+
+  // const getNextPageParam = (fetchImages) => {
+  //   console.log(fetchImages)
+  // }
+
   const {
     data,
     isLoading,
@@ -18,25 +28,25 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
+    fetchImages,
+    
     // TODO GET AND RETURN NEXT PAGE PARAM
   );
 
-  const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
-  }, [data]);
+  // const formattedData = useMemo(() => {
+  //   // TODO FORMAT AND FLAT DATA ARRAY
+  // }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  // // TODO RENDER LOADING SCREEN
 
-  // TODO RENDER ERROR SCREEN
+  // // TODO RENDER ERROR SCREEN
 
   return (
     <>
       <Header />
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
-        <CardList cards={formattedData} />
+        {/* <CardList cards={formattedData} /> */}
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
       </Box>
     </>
